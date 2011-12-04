4540a36c15e01a173b7122576cc5d1a42d1fa332
Mark Learst <learst@me.com>
2011-12-03 19:54:10 -0500

    player fix

--- _/js/functions.js
+++ _/js/functions.js
@@ -66,35 +66,35 @@ var myPlaylist = new jPlayerPlaylist({
 			title:"Alexandrine",
 			artist:"Millions of Brazilians",
 			mp3:"_/audio/alexandrine.mp3",
-			oga:"_/audio/alexandrine.ogg",
+			// oga:"_/audio/alexandrine.ogg",
 			poster: "_/img/albumart_497x497.jpg"
 		},
 		{	
 			title:"Fur In Winter",
 			artist:"FAWN",
 			mp3:"_/audio/fur_in_winter.mp3",
-			oga:"_/audio/fur_in_winter.ogg",
+			// oga:"_/audio/fur_in_winter.ogg",
 			poster: "_/img/albumart_497x497.jpg"
 		},
 		{
 			title:"Second Hand Holiday (ft. Jeff Tuttle and Chris Pennie)",
 			artist:"The Armed",
 			mp3:"_/audio/second_hand_holiday.mp3",
-			oga:"_/audio/second_hand_holiday.ogg",
+			// oga:"_/audio/second_hand_holiday.ogg",
 			poster: "_/img/albumart_497x497.jpg"
 		},
 		{
 			title:"What Child is This",
 			artist:"Daniel",
 			mp3:"_/audio/what_child_is_this.mp3",
-			oga:"_/audio/what_child_is_this.ogg",
+			// oga:"_/audio/what_child_is_this.ogg",
 			poster: "_/img/albumart_497x497.jpg"
 		},
 		{
 			title:"Winter Once Again",
 			artist:"Lightning Love",
 			mp3:"_/audio/winter_once_again.mp3",
-			oga:"_/audio/winter_once_again.ogg",
+			// oga:"_/audio/winter_once_again.ogg",
 			poster: "_/img/albumart_497x497.jpg"
 		}
 	], {
@@ -109,7 +109,8 @@ var myPlaylist = new jPlayerPlaylist({
 			shuffleTime: 'slow'
 		},
 			swfPath: "js",
-			supplied: "webmv, ogv, m4v, oga, mp3"
+			supplied: "mp3"
+			// supplied: "webmv, ogv, m4v, oga, mp3"
 	});
 });
 
